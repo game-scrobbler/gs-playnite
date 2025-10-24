53e5a154b2c3a54b2df3004c0e226f9081cbfb70
Mehrad <mehrad77@gmail.com>
2025-10-24 21:21:00 +0330

    chore(main): release GsPlugin 0.10.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 // x-release-please-start-version
-[assembly: AssemblyVersion("0.10.1")]
-[assembly: AssemblyFileVersion("0.10.1")]
+[assembly: AssemblyVersion("0.10.2")]
+[assembly: AssemblyFileVersion("0.10.2")]
 // x-release-please-end
